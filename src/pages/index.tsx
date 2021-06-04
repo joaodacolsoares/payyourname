b416609aea0ad94873ed52d607d8be13af269353
Joao D. Soares <joao.dacol.soares@gmail.com>
2021-06-04 14:43:29 -0300

    Added initial screen base to the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,22 @@
 import Head from 'next/head'
 
 export default function Home() {
+
   return (
-    <div>
+    <div className='bg-gray-800 text-white min-h-screen p-6 flex items-center flex-col'>
       <Head>
         <title>Eternity</title>
         <meta name="description" content="Eternity App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="w-screen h-screen flex items-center justify-center">
-        <h1 className="text-5xl">
-          Welcome to <strong>Eternity!</strong>
+      <div className='max-w-7xl w-full'>
+        <h1 className='text-5xl'>
+          Eternity
         </h1>
-      </main>
+      </div>
+      
+      
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Inter', 'Open Sans'],
+    },
+    extend: {}
   },
   variants: {
     extend: {},
