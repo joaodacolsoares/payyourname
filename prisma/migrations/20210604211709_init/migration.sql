bbad56bb9663bbae514881c8d3fe882c68e130c3
Joao D. Soares <joao.dacol.soares@gmail.com>
2021-06-04 19:03:04 -0300

    Added Prima into application

---
+++ prisma/migrations/20210604211709_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Nickname" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+
+    PRIMARY KEY ("id")
+);
---
+++ src/pages/api/nicknames.tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const nicknames = await prisma.nickname.findMany({
+    orderBy: [{
+      amount: 'desc'
+    }]
+  })
+  res.json(nicknames);
+}
