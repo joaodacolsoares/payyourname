cf04e44dcf5aad1f5631282069f5c3d5906e6b13
Joao D. Soares <joao.dacol.soares@gmail.com>
2021-06-08 07:56:00 -0300

    Added Poppins font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="favicon.png" type='image/png' />
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <Main/>
