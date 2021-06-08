2129755a67f3e51379b2a36de5c20cfbbf628fbe
João Soares <41935580+joaodacolsoares@users.noreply.github.com>
2021-06-08 19:34:12 -0300

    Updating to new style (#16)
    * Adding new styles to Tailwind
    
    * Added Poppins font
    
    * Changed style to fit design

--- src/components/RankingList.tsx
+++ src/components/RankingList.tsx
@@ -9,10 +9,10 @@ interface RankingListProps {
 export default function RankingList({ persons } : RankingListProps) {
   return (
     <>
-      <h2 className='mb-4 text-3xl'>Ranking</h2>
+      <h2 className='mb-4 text-4xl text-white font-bold'>Ranking</h2>
       <ol>
         {
-          persons.map(person => (<RankingListItem key={person.name} person={person} />))
+          persons.map((person, index) => (<RankingListItem key={person.name} person={person} index={index + 1} />))
         }
       </ol>
     </>
