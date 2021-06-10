be5f3d5f396cfd2a42d2f1e3e0fd682041e593d3
Joao D. Soares <joao.dacol.soares@gmail.com>
2021-06-10 19:00:01 -0300

    Create Nickname Context

---
+++ src/contexts/NicknameContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, ReactNode, useState } from "react";
+import Nickname from "../interfaces/Nickname";
+
+interface NicknameContextProps {
+  getTopNickname : () => Nickname | null,
+  getNicknameList : () => Nickname[]
+}
+
+export const NicknameContext = createContext({} as NicknameContextProps)
+
+interface NicknameProviderProps {
+  children: ReactNode,
+  nicknames: Nickname[]
+}
+
+export function NicknameProvider({ children, nicknames } : NicknameProviderProps ) {
+  const [nicknameList, setNicknames] = useState(nicknames || []);
+  
+  function getTopNickname() {
+    return nicknameList[0] || null
+  }
+
+  function getNicknameList() {
+    return nicknameList
+  }
+
+  return (
+    <NicknameContext.Provider value={{
+      getTopNickname,
+      getNicknameList
+    }}>
+      { children }
+    </NicknameContext.Provider>
+  )
+}
