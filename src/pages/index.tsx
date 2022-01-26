bf1790828176b9f1181e80e3ec61da5e6f64689a
Nicolas Prokopetz <59585323+Prokopetz@users.noreply.github.com>
2022-01-26 14:17:51 -0300

    fix: tab title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,8 +45,8 @@ export default function Home({ nicknameList }: HomeProps) {
     <NicknameProvider nicknames={nicknameList}>
       <div className='min-h-screen flex items-center flex-col bg-background scroll-snap-none relative'>
         <Head>
-          <title>Pay your name</title>
-          <meta name='description' content='Pay your name App' />
+          <title>Pay Your Name</title>
+          <meta name='description' content='Pay Your Name App' />
           <link rel='icon' href='/favicon.ico' />
         </Head>
 
