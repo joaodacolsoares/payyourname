624440bc2eb349df13dea2ab3851940289e79355
João Soares <41935580+joaodacolsoares@users.noreply.github.com>
2022-01-26 19:29:42 -0300

    fix: adjust favicon (#31)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap" rel="stylesheet"></link>
         </Head>
