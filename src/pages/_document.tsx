2791eb41bb2592ee9c1414f6791746de84c6a849
João Soares <41935580+joaodacolsoares@users.noreply.github.com>
2022-01-26 20:05:31 -0300

    feat: add analytics (#32)
    * chore: add analytics deps
    
    * feat: add analytics provider
    
    * feat: add track of events in home page
    
    * feat: add link to ga

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <script async src='https://www.google-analytics.com/analytics.js'></script>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
