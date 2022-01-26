c04d74e2dd81bfaf7394b04d813c737057e70585
Joao D. Soares <joao.dacol.soares@gmail.com>
2022-01-26 19:49:27 -0300

    feat: add analytics provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/index.css';
+import Analytics from 'analytics'
+import googleAnalytics from '@analytics/google-analytics'
+import { AnalyticsProvider, useAnalytics } from 'use-analytics'
+
+const analytics = Analytics({
+  app: 'pay-your-name',
+  plugins: [
+    googleAnalytics({
+      trackingId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+    })
+  ]
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AnalyticsProvider instance={analytics}>
+      <Component {...pageProps} />
+    </AnalyticsProvider>
+  )
 }
 
 export default MyApp
