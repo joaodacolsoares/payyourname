832e8064c3e0818379d21deb08994b36455e63c1
Joao D. Soares <joao.dacol.soares@gmail.com>
2022-01-26 19:57:46 -0300

    feat: add link to ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,10 @@ import 'tailwindcss/tailwind.css'
 import '../styles/index.css';
 import Analytics from 'analytics'
 import googleAnalytics from '@analytics/google-analytics'
-import { AnalyticsProvider, useAnalytics } from 'use-analytics'
+import { AnalyticsProvider } from 'use-analytics'
 
 const analytics = Analytics({
-  app: 'pay-your-name',
+  app: 'awesome-app',
   plugins: [
     googleAnalytics({
       trackingId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <script async src='https://www.google-analytics.com/analytics.js'></script>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
